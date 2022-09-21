126f0304a47a3916512cd326bd2ca259a02d26ee
Namkat Cedrick Jumtock <namkatcedrickjumtock3@gmail.com>
2022-09-21 03:29:41 +0100

    reverted changes with no auth implemented

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+// For more information on each option (and a full list of options) go to
+// https://next-auth.js.org/configuration/options
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    })
+  ],
+})
